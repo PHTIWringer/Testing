c304116f8e4e96d7fb92018b7d829c0ab8762603
Ken Hileman <ken.hileman@outlook.com>
2024-04-09 20:37:27 -0400

    challenge a solution

--- testing.py
+++ testing.py
@@ -546,9 +546,22 @@ sales_data = [
 
 #######################################################################################################################################################
 
+'''
 # Loop Challenge A: Create a Python script that prints all the numbers from 1 to 20, but with a twist. For each number:
 
 # If the number is divisible by 3, print "Fizz" instead of the number.
 # If the number is divisible by 5, print "Buzz" instead of the number.
 # If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
 
+for i in range(1, 21):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+'''
+
+#######################################################################################################################################################
